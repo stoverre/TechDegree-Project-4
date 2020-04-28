5c6bea242b6a20fb3be2f0aa68fea0b80a9b39be
stoverre <63378764+stoverre@users.noreply.github.com>
2020-04-28 19:24:24 -0400

    game fully works
    no extra credit included but all functionality has been tested through the UI

--- js/Game.js
+++ js/Game.js
@@ -22,6 +22,35 @@
         return [phrase0 , phrase1, phrase2, phrase3, phrase4]
     }
 
+     /**
+    * Begins game by selecting a random phrase and displaying it to user
+    */
+    startGame() {
+        //remove all 'li' elements from previous game
+        const ul = document.getElementById('phrase').firstElementChild
+        const length = ul.children.length
+        for(let i=0; i<length; i++){
+            ul.removeChild(ul.lastElementChild)
+        }
+        //enable all onscreen keyboard buttons
+        //indexed at 1 to not touch the reset button
+        const buttons = document.getElementsByTagName('button')
+        for(let i=1; i<26; i++){
+            if(buttons[i].id !== 'btn__reset'){
+                buttons[i].className = 'key'
+                buttons[i].disabled = false
+            }
+        }
+        //Reset lives
+        const lives = document.getElementsByTagName('img')
+        for(let i=0; i<5; i++){
+            lives[i].setAttribute('src', 'images/liveHeart.png')
+        }
+        document.getElementById('overlay').style.display = 'none'
+        this.activePhrase = this.getRandomPhrase()
+        this.activePhrase.addPhraseToDisplay()
+    };
+
     /**
     * Selects random phrase from phrases property
     * @return {Object} Phrase object chosen to be used
@@ -30,16 +59,7 @@
         const randomIndex = Math.floor(Math.random()*5)
         return this.phrases[randomIndex]
     }
-
-    /**
-    * Begins game by selecting a random phrase and displaying it to user
-    */
-    startGame() {
-        document.getElementById('overlay').style.display = 'none'
-        this.activePhrase = this.getRandomPhrase()
-        this.activePhrase.addPhraseToDisplay()
-    };
-
+    
     /**
     * Checks for winning move
     * @return {boolean} True if game has been won, false if game wasn't
@@ -79,7 +99,23 @@
 
     }
 
-    handleInteraction(){
+    /**
+    * Handles onscreen keyboard button clicks
+    * @param (HTMLButtonElement) button - The clicked button element
+    */
+    handleInteraction(button){
+        const letter = button.innerHTML
+        button.disabled = true
+        if(this.activePhrase.checkLetter(letter)){
+            button.className = 'chosen'
+            this.activePhrase.showMatchedLetter(letter)
+            if(this.checkForWin()){
+                this.gameOver(true)
+            }
 
+        }else{
+            button.className = 'wrong'
+            this.removeLife()
+        }
     }
  }
